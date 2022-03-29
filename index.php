5f1f0af3cc847672b6aab092d17b31e07238c68c
Yara Mohamed <102629489+theyara@users.noreply.github.com>
2022-03-30 00:46:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$names = array ("Ahmed","Mohamed","Fares","Mahmoud","Sela","Amira","Mina","Ben","Fras","NoOne");
+if((in_array("Sela" ,$names))or(in_array("none",$names))) 
+{
+    echo "exist" ;
+}
+else {
+    echo "failed" ;
+}
+
+
+
+
+
+
+
+
+
+
